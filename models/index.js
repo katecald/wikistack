8d7e62bafee184382469400ec50de7ef9a428d04
Allison Alexander <aveleber@hotmailcom>
2017-03-13 11:25:58 -0400

    sending all files

--- models/index.js
+++ models/index.js
@@ -1,17 +1,51 @@
 var Sequelize = require('sequelize');
-var db = new Sequelize('postgres://localhost:5432/wikistack');
+var db = new Sequelize('postgres://localhost:5432/wikistack', {
+	logging: false
+});
 
 var Page = db.define('page', {
-    title: Sequelize.STRING,
-    urlTitle: Sequelize.STRING,
-    content: Sequelize.TEXT,
-    status: Sequelize.BOOLEAN
-})
+    title: {
+    	type: 		Sequelize.STRING,
+    	allowNull: 	false
+
+    },
+    urlTitle: {
+    	type: 		Sequelize.STRING,
+    	allowNull: 	false
+    },
+
+    content: {
+    	type: 		Sequelize.TEXT,
+    	allowNull: 	false
+    	},
+
+    status: Sequelize.ENUM('open', 'closed'),
+
+    date: {
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.NOW
+    },
+
+    route: {
+    	type: Sequelize.STRING,
+    	get: function () {
+    		return '/wiki/' + this.urlTitle;
+    	}
+    }
+});
 
 var User = db.define('user', {
-    name: Sequelize.STRING,
-    email: Sequelize.STRING
-})
+    name: {
+    	type:   	Sequelize.STRING,
+    	allowNull: 	false
+    },
+
+    email: {
+    	type:   	Sequelize.STRING,
+    	allowNull: 	false,
+    	isEmail:    true 
+    }
+});
 
 module.exports = {
     Page: Page,
